6a4a7e6fb0391badf6dc5110f4b03dd3083988f9
Jeeva Kiran <venkatajeevakiran@gmail.com>
2018-12-13 16:18:21 +0530

    second commit

---
+++ Sring.java
@@ -0,0 +1,13 @@
+class Kt    
+{ public static void main(String[] args)
+{ String str1 = "hello";
+String str2 = "hello";
+String str3= new String("hello");
+System.out.println(str1==str2); //true
+System.out.println(str1==str3); //false
+System.out.println(str1==str3); //false
+System.out.println(str1.equals(str2)); //true
+System.out.println(str1.equals(str3)); //true
+System.out.println(str2.equals(str3)); //true
+}
+}
