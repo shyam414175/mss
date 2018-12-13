96e49a4a3457fce67519579b1411d7f974e25365
Jeeva Kiran <venkatajeevakiran@gmail.com>
2018-12-13 16:06:53 +0530

    Initial commit

---
+++ ggg.java
@@ -0,0 +1,10 @@
+class Arrr {
+    public static void main(String[]args)
+    {
+        String str="Krishnas";
+        System.out.println("str");
+        
+        String str1=new String("krishna");
+        System.out.println("str1");
+    }
+}
